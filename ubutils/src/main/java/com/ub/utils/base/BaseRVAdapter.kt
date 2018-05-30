faa9e861716f374b869c3d54b9fcfffb41a77c53
VO Lixanov <volixanov@unitbean.com>
2018-05-30 10:02:34 +0300

    - added click listener in BaseRVAdapter

--- ubutils/src/main/java/com/ub/utils/base/BaseRVAdapter.kt
+++ ubutils/src/main/java/com/ub/utils/base/BaseRVAdapter.kt
@@ -1,5 +1,17 @@
 package com.ub.utils.base
 
 import android.support.v7.widget.RecyclerView
+import android.view.View
 
-abstract class BaseRVAdapter<T : RecyclerView.ViewHolder> : RecyclerView.Adapter<T>()
+abstract class BaseRVAdapter<T : RecyclerView.ViewHolder> : RecyclerView.Adapter<T>() {
+
+    private var listener: BaseClickListener? = null
+
+    fun setListener(listener: BaseClickListener) {
+        this.listener = listener
+    }
+}
+
+interface BaseClickListener {
+    fun onClick(view: View, position: Int)
+}
