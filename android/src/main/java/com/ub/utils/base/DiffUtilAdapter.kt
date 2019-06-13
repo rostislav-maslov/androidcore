557c3e44caf44f51ec92e3b361e662c446b04f43
VO Lixanov <volixanov@unitbean.com>
2019-06-13 08:40:30 +0400

    - RecyclerView переведён на ListAdapter - обновлен Dagger2 до 2.23.1

--- android/src/main/java/com/ub/utils/base/DiffUtilAdapter.kt
+++ android/src/main/java/com/ub/utils/base/DiffUtilAdapter.kt
@@ -1,52 +1,21 @@
 package com.ub.utils.base
 
+import android.annotation.SuppressLint
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.ub.utils.renew
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ObsoleteCoroutinesApi
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.actor
-import kotlinx.coroutines.launch
-
 
 @Suppress("UNUSED")
-abstract class DiffUtilAdapter<D : DiffComparable, VH : RecyclerView.ViewHolder> : BaseRVAdapter<VH>(), CoroutineScope by CoroutineScope(Dispatchers.Main) {
+abstract class DiffUtilAdapter<D : DiffComparable, VH : RecyclerView.ViewHolder> : ListAdapter<D, VH>(DiffCallback<D>()) {
 
-    protected var dataset: MutableList<D> = mutableListOf()
-    private val diffCallback by lazy(LazyThreadSafetyMode.NONE) { DiffCallback() }
-    @ObsoleteCoroutinesApi
-    private val eventActor = actor<List<D>>(capacity = Channel.CONFLATED) {
-        for (list in channel)
-            internalUpdate(this@DiffUtilAdapter, list)
+    fun update(list: List<D>) {
+        this.submitList(list)
     }
 
-    @ObsoleteCoroutinesApi
-    fun update(list: List<D>) = eventActor.offer(list)
-
-    fun updateSync(list: List<D>) {
-        diffCallback.newList.renew(list)
-        DiffUtil.calculateDiff(diffCallback).dispatchUpdatesTo(this)
-        dataset.renew(list)
-    }
-
-    private suspend fun internalUpdate(scope: CoroutineScope, list: List<D>) {
-        val result = DiffUtil.calculateDiff(diffCallback.apply {
-            newList.renew(list)
-        })
-        scope.launch {
-            dataset.renew(list)
-            result.dispatchUpdatesTo(this@DiffUtilAdapter)
-        }.join()
-    }
-
-    private inner class DiffCallback : DiffUtil.Callback() {
-        internal var newList: MutableList<D> = mutableListOf()
-        override fun getOldListSize() = dataset.size
-        override fun getNewListSize() = newList.size
-        override fun areItemsTheSame(oldItemPosition : Int, newItemPosition : Int) = dataset[oldItemPosition].getItemId() == newList[newItemPosition].getItemId()
-        override fun areContentsTheSame(oldItemPosition : Int, newItemPosition : Int) = dataset[oldItemPosition] == newList[newItemPosition]
+    @SuppressLint("DiffUtilEquals")
+    private class DiffCallback<D : DiffComparable> : DiffUtil.ItemCallback<D>() {
+        override fun areContentsTheSame(oldItem: D, newItem: D): Boolean = oldItem == newItem
+        override fun areItemsTheSame(oldItem: D, newItem: D): Boolean = oldItem.getItemId() == newItem.getItemId()
     }
 }
 
