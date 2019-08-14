e95f7d12d139c5f6e265ccd5c380e81c0d2e33a9
VO Lixanov <volixanov@unitbean.com>
2019-08-14 16:20:01 +0400

    - добавлен слушатель на клик по ListAdapter'у

--- android/src/main/java/com/ub/utils/base/BaseListAdapter.kt
+++ android/src/main/java/com/ub/utils/base/BaseListAdapter.kt
@@ -1,6 +1,7 @@
 package com.ub.utils.base
 
 import android.annotation.SuppressLint
+import android.view.View
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 abstract class BaseListAdapter<D : DiffComparable, VH : RecyclerView.ViewHolder> : ListAdapter<D, VH>(DiffCallback<D>()) {
 
     var listener: BaseClickListener? = null
+    var listListener: BaseListClickListener<D>? = null
 
     fun update(list: List<D>) {
         this.submitList(list)
@@ -18,5 +20,9 @@ abstract class BaseListAdapter<D : DiffComparable, VH : RecyclerView.ViewHolder>
     private class DiffCallback<D : DiffComparable> : DiffUtil.ItemCallback<D>() {
         override fun areContentsTheSame(oldItem: D, newItem: D): Boolean = oldItem == newItem
         override fun areItemsTheSame(oldItem: D, newItem: D): Boolean = oldItem.getItemId() == newItem.getItemId()
+    }
+
+    interface BaseListClickListener<D> {
+        fun onClick(view: View, item: D, position: Int)
     }
 }
